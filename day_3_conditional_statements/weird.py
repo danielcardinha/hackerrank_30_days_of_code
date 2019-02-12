5f8d779398489b3c132faad6250c4949bb801e5b
Saida Caunhye <saida.caunhye@inmarsat.com>
2019-02-12 17:02:13 +0000

    Complete the Weird function

--- day_3_conditional_statements/weird.py
+++ day_3_conditional_statements/weird.py
@@ -4,3 +4,9 @@ def weird(value):
     # If remainder is not 0, then its an odd number, then return Weird
     if remainder != 0:
         return "Weird"
+    elif value in (2, 4):
+        return "Not Weird"
+    elif 6 <= value <= 20:
+        return "Weird"
+    else:
+        return "Not Weird"
